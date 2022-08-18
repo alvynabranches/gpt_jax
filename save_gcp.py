e6697c653beb891b8f88b92b91f727d80b750255
Alvyn Abranches <alvynabranches@gmail.com>
2022-08-18 14:11:51 +0530

    Updated GCP Files

--- save_gcp.py
+++ save_gcp.py
@@ -10,7 +10,7 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "keys.json"
 
 def upload_to_bucket(blob_name, file_path, file_start_name, bucket_name):
     file = os.path.join(file_path, [file for file in os.listdir(file_path) if file.startswith(file_start_name)][0])
-    print(file)
+    # print(file)
     try:
         client = storage.Client()
         bucket = client.get_bucket(bucket_name)
@@ -29,6 +29,6 @@ if __name__ == '__main__':
     parser.add_argument("--file_start_name")
     args = parser.parse_args()
     
-    print(f"{args.blob_name=}\n{args.bucket_name=}\n{args.file_path=}\n{args.file_start_name=}")
+    # print(f"{args.blob_name=}\n{args.bucket_name=}\n{args.file_path=}\n{args.file_start_name=}")
     out = upload_to_bucket(args.blob_name, args.file_path, args.file_start_name, args.bucket_name)
     print(out)
